eef5ed04f771b04bd76c2c36d42c36a3bcc7aadb
viniciusgrangeiro <vinicius.grangeiro@gmail.com>
2025-01-29 16:36:38 -0300

    docs: add some comments

--- src/br/com/alura/screenmatch/calculations/RecomendationFilter.java
+++ src/br/com/alura/screenmatch/calculations/RecomendationFilter.java
@@ -1,10 +1,24 @@
 package br.com.alura.screenmatch.calculations;
 
+/**Classe para objetos do tipo RecomendationFilter, onde existira apenas um metodo.
+ * @author Vinicius Grangeiro
+ * @version 1.0
+ * @since Release 0 a aplicação
+ */
+
+
 public class RecomendationFilter {
 
-    // Este metodo ira filtrar um título "Classificavel"
-    // Dependendo do título, ele tera seu metodo de getClassification,
-    // que por sua vez, retornara um valor de avaliacao
+    /**Metodo para filtrar um titulo e informar se ele é bem avaliado
+     *
+     * Este metodo ira filtrar um título "Classificavel"
+     * Dependendo do título, ele tera seu metodo de getClassification,
+     * que por sua vez, retornara um valor de avaliacao
+     *
+     * @author Vinicius Grangeiro
+     * @param classifiable Classifiable - Titulo classificavel
+     * @return String - Imprime a classificacao do titulo
+     */
     public void filterTitle(Classifiable classifiable){
         if (classifiable.getClassification() >= 4){
             System.out.println("Esta entre os preferidos da plataforma");
